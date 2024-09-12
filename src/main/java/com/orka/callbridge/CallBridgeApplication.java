ee91238eec923469c7a934f074d9f81ca50e8e93
Orka2024 <itteamorkaitsolutions@gmail.com>
2024-09-12 21:12:47 +0530

    first commit

---
+++ src/main/java/com/orka/callbridge/CallBridgeApplication.java
@@ -0,0 +1,14 @@
+package com.orka.callbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallBridgeApplication.class, args);
+		System.out.println("Hello CallBridge in SpringBoot.......!");
+	}
+
+}
