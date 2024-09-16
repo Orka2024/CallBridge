42c2ffef508c51a333cd0c136bbbf0b5ef6f455a
Orka2024 <itteamorkaitsolutions@gmail.com>
2024-09-16 10:52:28 +0530

    Updated Structure 16-09-2024

---
+++ src/main/java/com/orka/callbridge/dao/UserRepository.java
@@ -0,0 +1,13 @@
+package com.orka.callbridge.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.orka.callbridge.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	@Query("select u from User u where u.email = :email ")
+	public User getUserByUsername(@Param("email") String email);
+
+}
---
+++ src/main/java/com/orka/callbridge/helper/Message.java
@@ -0,0 +1,5 @@
+package com.orka.callbridge.helper;
+
+public class Message {
+
+}
