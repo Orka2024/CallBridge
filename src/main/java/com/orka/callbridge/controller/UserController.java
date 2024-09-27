8d1422c8214d60789a229eeca977d689ccf673fc
Orka2024 <itteamorkaitsolutions@gmail.com>
2024-09-27 14:45:53 +0530

    Created makecall calling table page 27/09/2024

--- src/main/java/com/orka/callbridge/controller/UserController.java
+++ src/main/java/com/orka/callbridge/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.orka.callbridge.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-	@RequestMapping(value = "/caller/dashboard")
+	@RequestMapping(value = "/dashboard")
 	public String callerDashboard() {
 		return "pages/dashboard";
 	}
@@ -41,6 +42,48 @@ public class UserController {
 	@GetMapping("/calling")
 	public String callingClients() {
 		return "pages/CallersCalling";
+	}
+	
+	@GetMapping("/InterestCust_table")
+	public String InterCust_table( Model model) {
+		return "pages/InterestCustomer_table";
+	}
+
+
+	@GetMapping("/applycibil")
+	public String CreateForm(Model model) {  
+		return "pages/applycibilform";
+	
+	}
+
+	@GetMapping("/cibilmis")
+	public String Miscibil(Model model) {  
+		return "pages/applycibilmis";
+	
+	}
+	
+	@GetMapping("/activelist")
+	public String activelistmis(Model model) {  
+		return "pages/activecasemis";
+	}
+
+	
+	
+	@GetMapping("/approvemis")
+	public String approvelist(Model model) {  
+		return "pages/allapprovemis";
+	}
+
+	
+	@GetMapping("/rejectmis")
+	public String rejectlist(Model model) {  
+		return "pages/allrejectmis";
+	}
+	
+	
+	@GetMapping("/holdmis")
+	public String holdlist(Model model) {  
+		return "pages/allholdmis";
 	}
 
 }
