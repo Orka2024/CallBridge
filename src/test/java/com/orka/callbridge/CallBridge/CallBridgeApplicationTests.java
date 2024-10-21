93a2d5528f93144a541e9b6d21bd2205d201115f
Orka2024 <itteamorkaitsolutions@gmail.com>
2024-10-21 14:59:26 +0530

    Revert "work into email send to  testclient 21-10-2024 2.50"

--- src/test/java/com/orka/callbridge/CallBridge/CallBridgeApplicationTests.java
+++ src/test/java/com/orka/callbridge/CallBridge/CallBridgeApplicationTests.java
@@ -1,10 +1,7 @@
 package com.orka.callbridge.CallBridge;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.orka.callbridge.service.EmailService;
 
 @SpringBootTest
 class CallBridgeApplicationTests {
@@ -13,16 +10,4 @@ class CallBridgeApplicationTests {
 	void contextLoads() {
 	}
 
-	
-//	@Autowired
-//	private EmailService service;
-//	
-//	@Test
-//	void sendEmailTest() {
-//		service.sendEmail(
-//				"raviotpsend@gmail.com",
-//				"just test msg send ",
-//				"I want to send email raviotpsend@gamil.com");
-//		
-//	}
 }
